89a133b004b9f6d59fdbf12dcda7be11d8146087
Ash <53407318+ashthemagnificent@users.noreply.github.com>
2021-09-03 17:09:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Bölüm_8_ödev_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("The wage: ");
+            double wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The raise: ");
+            double raise = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The number of year: ");
+            double year = Convert.ToDouble(Console.ReadLine());
+
+            for (double counter = 0; counter < year; counter++)
+
+            {
+                wage = (wage + wage * raise/100);
+            }
+
+            double NewWage = wage;
+
+            
+            Console.Clear();
+            Console.WriteLine("The Latest Wage: {0}", wage);
+            Console.ReadLine();
+
+
+
+        
+
+
+
+        }
+    }
+}
